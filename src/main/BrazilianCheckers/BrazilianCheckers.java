920a007277b3ea28a7c148853dc66056d9b19b71
SilverSabre92 <lczapraga@gmail.com>
2019-08-27 21:38:54 +0200

    Version 0.0.1 - Board created

--- src/main/BrazilianCheckers/BrazilianCheckers.java
+++ src/main/BrazilianCheckers/BrazilianCheckers.java
@@ -3,6 +3,7 @@ package main.BrazilianCheckers;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -12,7 +13,7 @@ import java.awt.*;
 
 public class BrazilianCheckers extends Application{
 
-
+    private Image imageback = new Image("Checkersboard.jpg");
 
     public static void main (String[] args){
         launch(args);
@@ -20,8 +21,17 @@ public class BrazilianCheckers extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true,true, false);
-        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
-        Background background = new Background(backgroundImage);)
+        BackgroundSize backgroundSize = new BackgroundSize(200, 200, true,true, true,false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene (grid, 1600, 900, Color.BLACK);
+
+        primaryStage.setTitle("BrazilianCheckers");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
